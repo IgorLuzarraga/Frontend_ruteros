9566d0903dff130d142955a33d316d6773c80385
Igor <iluzarraga@gmail.com>
2023-08-10 17:30:30 +0200

    Project Ruteros FrontEnd

---
+++ src/services/API_proyect/service.config.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+import { updateToken } from '../../util/updateToken';
+
+const APIHeaders = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  Authorization: `Bearer ${updateToken()}`,
+};
+
+export const API = axios.create({
+  baseURL: `http://localhost:8080/api/v1`,
+  // baseURL: `https://backend-ruteros.vercel.app/api/v1`,
+  baseURL: `https://backend-ruteros-eta.vercel.app/api/v1`,
+
+  headers: APIHeaders,
+  timeout: 60000,
+});
+
+//Original baseURL in line 12: baseURL: `https://backend-dev-link.vercel.app/api/v1`,
